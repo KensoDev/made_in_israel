4c29a9908da6698de8635e50365252d7584af00d
Avi Tzurel <avi@kensodev.com>
2012-05-18 12:58:13 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MadeInIsrael::Application.config.secret_token = 'a7e03413ba50f4a09c20fe0edf229fad9dacd9ae624a7d8c657dee5e3d53c35b780b09e5260fd810ee2f6a74ed9be9674f7f18495f635a2bd9fbf30caa582960'
